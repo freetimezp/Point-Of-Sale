69e4ee03f8d2e16c18ffc14ae8897f5ce544e294
freetimezp <kraftukrnet@ukr.net>
2022-06-14 20:14:32 +0300

    18. Point of sale. Database class.

--- app/core/database.php
+++ app/core/database.php
@@ -1,1 +1,37 @@
 <?php
+
+class Database
+{
+    private function db_connect() {
+        $DBHOST = "localhost";
+        $DBNAME = "point_of_sale_db";
+        $DBUSER = 'root';
+        $DBPASS = '';
+        $DBDRIVER = "mysql";
+
+
+        try{
+            $con = new PDO("$DBDRIVER:host=$DBHOST;dbname=$DBNAME;", $DBUSER, $DBPASS);
+        }catch(PDOException $e) {
+            $e->getMessage();
+        }
+
+        return $con;
+    }
+
+    public function query($query, $data = array()) {
+        $con = $this->db_connect();
+        $smt = $con->prepare($query);
+        $check = $smt->execute($data);
+
+        if($check) {
+            $result = $smt->fetchAll(PDO::FETCH_ASSOC);
+
+            if(is_array($result) && count($result) > 0) {
+                return $result;
+            }
+        }
+
+        return false;
+    }
+}
--- app/core/functions.php
+++ app/core/functions.php
@@ -1,38 +1,5 @@
 <?php
 
-function db_connect() {
-    $DBHOST = "localhost";
-    $DBNAME = "point_of_sale_db";
-    $DBUSER = 'root';
-    $DBPASS = '';
-    $DBDRIVER = "mysql";
-
-
-    try{
-        $con = new PDO("$DBDRIVER:host=$DBHOST;dbname=$DBNAME;", $DBUSER, $DBPASS);
-    }catch(PDOException $e) {
-        $e->getMessage();
-    }
-
-    return $con;
-}
-
-function query($query, $data = array()) {
-    $con = db_connect();
-    $smt = $con->prepare($query);
-    $check = $smt->execute($data);
-
-    if($check) {
-        $result = $smt->fetchAll(PDO::FETCH_ASSOC);
-
-        if(is_array($result) && count($result) > 0) {
-            return $result;
-        }
-    }
-
-    return false;
-}
-
 function show($data) {
     echo "<pre>";
     print_r($data);
