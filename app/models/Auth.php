50f2d94dd96c2b9f5cc768414223ef3f9984ffcd
freetimezp <kraftukrnet@ukr.net>
2022-07-12 20:03:32 +0300

    44. Point of sale. Auth class create,

---
+++ app/models/Auth.php
@@ -0,0 +1,38 @@
+<?php
+
+class Auth
+{
+    public static function get($column) {
+        if(!empty($_SESSION['USER'][$column])) {
+            return $_SESSION['USER'][$column];
+        }
+
+        return 'Hi, User';
+    }
+
+    public static function lpgged_in() {
+        if(!empty($_SESSION['USER'])) {
+            $db = new Database();
+            if($db->query("SELECT * FROM users WHERE email = :email LIMIT 1", ['email' => $_SESSION['USER']['email']])) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static function access($role) {
+        $access['admin'] = ['admin'];
+        $access['accountant'] = ['admin', 'accountant'];
+        $access['supervisor'] = ['admin', 'supervisor'];
+        $access['cashier'] = ['admin', 'supervisor', 'cashier'];
+        $access['user'] = ['admin', 'supervisor', 'cashier', 'user'];
+
+        $myrole = self::get('role');
+        if(in_array($myrole, $access[$role])) {
+            return true;
+        }
+
+        return false;
+    }
+}
